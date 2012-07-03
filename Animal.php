6e38c314c7f8f97bdf4810f59a8816c1d85d0c8d
fisher2 <sog_97@mail.bg>
2012-07-03 16:36:49 +0300

    Update master

--- Animal.php
+++ Animal.php
@@ -8,7 +8,7 @@ class Animal {
     public function getResult($a, $b) {
         $this->a = $a;
         $this->b = $b;
-        echo $this->a +this->b;
+        echo $this->a + $this->b;
         return 1;
     }
 
