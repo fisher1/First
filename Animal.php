7a45e434166d5f6b1e2d87b18599dc403cc94fe4
fisher1 <svilen.grigorov97@gmail.com>
2012-07-03 15:57:13 +0300

    added new files

---
+++ Animal.php
@@ -0,0 +1,14 @@
+<?php
+
+class Animal {
+
+    private $av;
+    private $bav;
+
+    public function getResult($a, $b) {
+        $this->a = $a;
+        $this->b = $b;
+        echo $this->a + $this->b;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title></title>
+        <meta content="">
+        <style></style>
+    </head>
+    <body>
+        <?php
+        include("Animal.php");
+        $animal = new Animal();
+        $animal->getresult(4, 8);
+        if (2 == 2) {
+            echo 3;
+        }
+        ?>
+    </body>
+</html>
